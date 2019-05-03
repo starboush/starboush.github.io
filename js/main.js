993c81622ef9d26fc8b7a178e83fb8754a12415e
starboush <suhaip.2@gmail.com>
2019-05-03 20:45:53 +0300

    change

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+/*var ftext = document.getElementById("ftext").value;
+var ttext = document.getElementById('ttext').value;
+var button = document.getElementById('button');
+button.onclick = function() {
+   var sum = 0;
+   for(var i = ftext; i<= ttext; i++){
+       if(i%2 == 0){
+           sum += i;
+           document.writeln(i);
+       }
+   }
+    document.writeln(sum);
+}*/
